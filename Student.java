779a29334e03522bfe2f982ed621d12d3f6566fe
game168 <57764107+game168@users.noreply.github.com>
2019-11-15 00:15:39 +0800

    123

---
+++ Student.java
@@ -0,0 +1,32 @@
+
+public class Student {
+	private String name;
+	private String studentId;
+	private String department;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getStudentId() {
+		return this.studentId;
+	}
+	
+	public String getdepartment() {
+		return this.department;
+	}
+	
+	
+}
